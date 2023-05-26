390a33eb4ad14d84e3b8444826c86a075378cfbe
Tauqeer <hellotauqeer@gmail.com>
2023-05-26 23:58:50 +0530

    added hashed password function

--- models/userModel.js
+++ models/userModel.js
@@ -28,7 +28,15 @@ const userSchema = new mongoose.Schema({
     }
 }); 
 
-
+//hashed password 
+userSchema.pre('save', async function(next){
+    if(!this.isModified('password')){
+        next()
+    }
+    const salt = await bcrypt.genSalt(10)
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+}); 
 const User = mongoose.model('User', userSchema);
 
 module.exports = User; 
