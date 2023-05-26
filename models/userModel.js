dede601808e8f60b74d23adb92ade5a0a11ced9d
Tauqeer <hellotauqeer@gmail.com>
2023-05-26 23:49:56 +0530

    added userSchema

--- models/userModel.js
+++ models/userModel.js
@@ -3,7 +3,31 @@ const bcrypt = require('bycrypt');
 const JWT = require('jsonwebtoken'); 
 
 //models 
-const userSchema = new mongoose.Schema({}); 
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String, 
+        required: [true, 'Username is Required']
+    }, 
+    email: {
+        type: String, 
+        required:[true, 'Email is required'], 
+        unique:true
+    },
+    password: {
+        type: String,  
+        required:[true, 'Password is required'], 
+        minlength:[6, 'Password length should be 6 character length']
+    },
+    customerId:{
+        type: String, 
+        default:""
+    }, 
+    subscription: {
+        type: String,
+        defualt: "", 
+    }
+}); 
+
 
 const User = mongoose.model('User', userSchema);
 
