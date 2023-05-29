dd36af9264f81901898c03a9c47cedc9b22b9de1
Tauqeer <hellotauqeer@gmail.com>
2023-05-29 23:49:15 +0530

    Added Pages and Components

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+      <h1>Nav Menu</h1>
+    </div>
+  )
+}
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Homepage() {
+  return (
+    <div>
+      <h1>Nav Menu</h1>
+    </div>
+  )
+}
