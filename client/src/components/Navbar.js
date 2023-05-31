d00c8df2de6bedf0079fd18addfbf1797f55886b
Tauqeer <hellotauqeer@gmail.com>
2023-05-31 16:59:43 +0530

    developed Navbar content with Box and heading

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { Box, Link, Typography, useTheme } from "@mui/material";
 
 export default function Navbar() {
-  return (
-    <div>
-      <h1>Nav Menu</h1>
-    </div>
-  )
+  const theme = useTheme();
+  return (    
+      <Box
+        width={"100%"}
+        backgroundColor = {theme.palette.background.alt}
+        p="1rem 6%"
+        textAlign={"center"}
+        sx={{ boxShadow: 3, mb: 2}}
+      >
+        <Typography variant="h1" color={"primary"} fontWeight="bold">
+          AI GPT3 Clone
+        </Typography>
+        <Link href="/register" p={1}>
+          Sign Up
+        </Link>
+        <Link href="/login" p={1}>
+          Sign In
+        </Link>
+        <h1>Nav Menu</h1>
+      </Box>
+  );
 }
